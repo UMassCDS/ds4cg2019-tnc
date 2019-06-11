55d6c5b9fb4712f34ebda3dd4b27c030cd2bb977
won-bae <bwh0324@gmail.com>
2019-06-11 16:18:00 +0000

    added lr schedule

--- src/builders/optimizer_builder.py
+++ src/builders/optimizer_builder.py
@@ -9,21 +9,25 @@ OPTIMIZERS = {
 
 def build(train_config, model_params, checkpoint):
     if 'optimizer' not in train_config:
-        log.error('Specify an optimizer')
-        exit()
+        log.error('Specify an optimizer'); exit()
 
     optim_config = train_config['optimizer']
     optimizer_name = optim_config.pop('name', 'sgd')
+    lr_schedule = optim_config.pop('lr_schedule', None)
     optim_config['params'] = model_params
 
     if optimizer_name in OPTIMIZERS:
         optimizer = OPTIMIZERS[optimizer_name](**optim_config)
     else:
-        log.error('Enter valid optimizer name among {}'.format(OPTIMIZERS))
-        exit()
+        log.error('Specify valid optimizer name among {}'.format(OPTIMIZERS)); exit()
+
+    if lr_schedule is not None:
+        lr_schedule['optimizer'] = optimizer
+        # TODO: check if it's okay to assgin the same name as input variable
+        optimizer = optim.lr_scheduler.StepLR(**lr_schedule)
 
     if checkpoint is not None:
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-
     log.infov('{} optimizer is built'.format(optimizer_name.upper()))
+
     return optimizer
