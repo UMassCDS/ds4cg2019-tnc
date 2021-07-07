9c24de63cac983faecc95023e389fa60fe211073
agr1997 <g.arvind1@gmail.com>
2021-07-07 06:08:00 -0400

    flask update. refer tnc2. add pb file

--- flask/TNC2/app/app/__init__.py
+++ flask/TNC2/app/app/__init__.py
@@ -4,6 +4,6 @@ from flask_dropzone import Dropzone
 app = Flask(__name__)
 dropzone = Dropzone(app)
 
-from app import model_server_test
+# from app import model_server_test
 from app import web_server_test
 
