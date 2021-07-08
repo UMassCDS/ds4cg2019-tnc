cd7f4d9aa4a223aec37df2dceb16beb72de3c040
agr1997 <g.arvind1@gmail.com>
2021-07-08 02:44:31 -0400

    requirements.txt updated

--- flask/TNC2/app/app/model_server_test.py
+++ flask/TNC2/app/app/model_server_test.py
@@ -1,22 +1,36 @@
-import tensorflow as tf
 import numpy as np
+import pandas
 import redis
-import os, pathlib, uuid, ntpath, random, time, json
+import os, pathlib, uuid, ntpath, random, time, json, sys
+import csv
+from zipfile import ZipFile
 from PIL import Image
+from PIL.ExifTags import TAGS
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import matplotlib.pyplot as plt 
 import matplotlib.patches as patches
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1" # remove if using GPU
+
+import tensorflow as tf
 
 # Config parameters
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 REDIS_DB = 0
 
-IMAGE_QUEUE = "image_queue"
 IMAGE_HEIGHT = 1920 * 0.75
 IMAGE_WIDTH = 1920
 IMAGE_DTYPE = "float32"
+UPLOAD_PATH = os.path.join(os.getcwd(), "static/uploaded_img/")
+DOWNLOAD_PATH = os.path.join(os.getcwd(), "static/detected_img/")
 
-BATCH_SIZE = 1 # Change only when Tensorflow GPU is used
+USER_QUEUE = "user_queue"
+
+BATCH_SIZE = 4 # Change only when Tensorflow GPU is used
+
 
 # Connecting to Redis server
 db = redis.StrictRedis(host=REDIS_HOST, 
@@ -27,11 +41,16 @@ def filename_extractor(path):
     head, tail = ntpath.split(path)
     return tail or ntpath.basename(head)
 
+# To make hyperlinks to file system paths
+def clickable(path):
+    f_url = os.path.basename(path)
+    return '=HYPERLINK("{}", "{}")'.format(path, f_url)
+
 # Load megadetector_v3.pb
 def load_graph():
-    sess = tf.Session()
-    with tf.gfile.GFile(os.path.join(os.getcwd(), "megadetector_v3.pb"), 'rb') as tf_graph:
-        graph_def = tf.GraphDef()
+    sess = tf.compat.v1.Session()
+    with tf.io.gfile.GFile(os.path.join(os.getcwd(), "megadetector_v3.pb"), 'rb') as tf_graph:
+        graph_def = tf.compat.v1.GraphDef()
         graph_def.ParseFromString(tf_graph.read())
         tf.import_graph_def(graph_def, name='')
     # label_lines = [line.rstrip() for line in tf.gfile.GFile(TF_LABELS)]
@@ -47,89 +66,153 @@ def load_graph():
 # classify from redis as and when available
 def tf_classify():
     # loading model
+    start = time.time()
     sess, input_tensor, output_tensors = load_graph() 
-    predictions = []
+    end = time.time()
+    print("Model loading completed in " + str(end - start))
 
     # continuosly polling for data
     while True:
-        # attempting to grab userIDs from Redis databases
-        queue = db.lrange(IMAGE_QUEUE, 0, BATCH_SIZE - 1)
-        # UUID list to store and retrieve images
-        imageIDs = [] 
-        userID = None
-        for q in queue:
-            q = json.loads(q)
-            print(type(q))
-            # loop over the queue to get all images
-            for img in q["images"]:
-                imageIDs.append(img)
-            userID = q["user_id"]
-        
-        # checking too see if there are images on redis to classify 
-        if len(imageIDs) > 0:
-            print("Running classifier")
-            # Save predictions for all images as a list
+        # attempting to grab any user request from Redis databases
+        user = db.lrange(USER_QUEUE, 0, 0)
+        finalnames = []
+        if user:
+            # If exists, get ID and email
+            userID = json.loads(user[0])["userID"]
+            email = json.loads(user[0])["email"]
+            queue = db.lrange(userID, 0, BATCH_SIZE - 1)
             predictions = []
-            for i in range(len(imageIDs)):
-                # filepath = os.path.join(os.getcwd(), "app/static/uploaded_img/", imageIDs[i])
+            while queue:
+                images = []
+                imagenames = []
+                imtags = []
+                for img in queue:
+                    # loop over the queue to get all preprocessed images
+                    img = json.loads(img)["image"]
+                    imagenames.append(img)
+                    image = Image.open(os.path.join(UPLOAD_PATH, img))
+                    imtags.append(image._getexif())
+                    image = image.convert("RGB")
+                    image = image.resize((IMAGE_WIDTH, int(IMAGE_HEIGHT)))
+                    # image = np.expand_dims(image, axis=0) 
+                    images.append(np.array(image)) # Appending instead of expanding since we want batch processing
+        
+                # checking to see if we should run classifer (redundant)
+                if len(images) > 0:
+                    print("Running classifier for ", len(images))
+                    # Save predictions for all images as a list
+                    
 
-                # Can preprocess image during upload process but for now
-                # let's make the model run the preprocessing
-                image = Image.open(imageIDs[i]).convert("RGB")
-                image = image.resize((IMAGE_WIDTH, int(IMAGE_HEIGHT)))
-                image = np.expand_dims(image, axis=0)
+                    # Run model and obtain tensor scores on batch of images
+                    images = np.asarray(images)
+                    start = time.time()
+                    boxes, scores, classes = sess.run(output_tensors, {input_tensor: images})
+                    end = time.time()
+                    print("Model ran in " + str(end - start))
+                    # Check each image for passing criteria and construct bounding box if need be
+                    for i in range(len(images)):
+                        start = time.time()
+                        above_threshold = scores[i] > 0.5
+                        boxes_above_threshold = boxes[i][above_threshold]
+                        classes_above_threshold = classes[i][above_threshold]
+                        scores_above_threshold = scores[i][above_threshold]
+                        if not np.any(scores_above_threshold):
+                            pred = {"image": imagenames[i], "label":"None", "score":"0"}
+                            predictions.append(pred)
+                        else:
+                            is_animal = np.sum(classes_above_threshold == 1) > 0
+                            max_score = np.max(scores_above_threshold)
+                            label = ["None", "Animal/Human"]
+                            pred = {"image":imagenames[i], "label": label[is_animal], "score": str(max_score)}
+                            predictions.append(pred)
+                        print(predictions)
+                        # Adding predictions to userID_PRED queue
+                        db.rpush(userID + '_PRED', json.dumps(pred))
 
-                # Run model and obtain tensor scores
-                boxes, scores, classes = sess.run(output_tensors, {input_tensor: image})
+                        # Constructing bounding box on image file
+                        fig, ax = plt.subplots(1)
+                        ax.imshow(images[i].squeeze())
+                        if np.any(boxes_above_threshold):
+                            # Add imagename to finalnames
+                            finalnames.append(imagenames[i])
+                            for box, cls in zip(boxes_above_threshold, classes_above_threshold):
+                                ymin, xmin, ymax, xmax = box
+                                x = xmin * IMAGE_WIDTH
+                                y = ymin * IMAGE_HEIGHT
+                                w = (xmax - xmin) * IMAGE_WIDTH
+                                h = (ymax - ymin) * IMAGE_HEIGHT
+                                edgecolor = 'g' if cls == 1 else 'r'
+                                rect = patches.Rectangle((x, y), w, h, linewidth=5,
+                                                        edgecolor=edgecolor, facecolor='none')
+                                ax.add_patch(rect)
 
-                # Check for passing criteria 
-                above_threshold = scores > 0.35
-                boxes_above_threshold = boxes[above_threshold]
-                classes_above_threshold = classes[above_threshold]
-                scores_above_threshold = scores[above_threshold]
+                            # Saving file to location on disk
+                            plt.axis('off')
+                            if imtags[i]:
+                                plt.savefig(os.path.join("static/detected_img/", imagenames[i]), pil_kwargs={"exif":imtags[i]}, dpi=100, transparent=True, optimize=True, quality=90)
+                            else:
+                                plt.savefig(os.path.join("static/detected_img/", imagenames[i]), dpi=100, transparent=True, optimize=True, quality=90)
+                        end = time.time()
+                        print("Image saved in " + str(end-start))
+                    
+                    # Remove processed images from user queue
+                    db.ltrim(userID, BATCH_SIZE, -1)
+                # Update queue for next round
+                queue = db.lrange(userID, 0, BATCH_SIZE - 1)
 
-                is_animal = np.sum(classes_above_threshold == 1) > 0
-                max_score = np.max(scores_above_threshold)
-                predictions.append([str(imageIDs[i]), "Yes", max_score]) # ([LABELS[int(is_animal)], max_score])
-                # print(predictions)
+            # Make a dataframe of predictions and convert to excel sheet
+            df = pandas.DataFrame(predictions)
+            print(df)
+            df['image'] = df['image'].apply(lambda x: clickable(x))
+            df.to_excel(os.path.join(DOWNLOAD_PATH, userID + ".xlsx"), encoding='utf-8', index=False)
 
-                # Constructing bounding box on image file
-                fig, ax = plt.subplots(1)
-                ax.imshow(image.squeeze())
-                for box, cls in zip(boxes_above_threshold, classes_above_threshold):
-                    ymin, xmin, ymax, xmax = box
-                    x = xmin * IMAGE_WIDTH
-                    y = ymin * IMAGE_HEIGHT
-                    w = (xmax - xmin) * IMAGE_WIDTH
-                    h = (ymax - ymin) * IMAGE_HEIGHT
-                    edgecolor = 'g' if cls == 1 else 'r'
-                    rect = patches.Rectangle((x, y), w, h, linewidth=5,
-                                            edgecolor=edgecolor, facecolor='none')
-                    ax.add_patch(rect)
+            # with open(DOWNLOAD_PATH + userID + ".csv", "w+") as my_csv:
+            #     csvWriter = csv.writer(my_csv, delimiter=',')
+            #     csvWriter.writerows(predictions)
 
-                # Saving file to location on disk
-                plt.axis('off')
-                plt.savefig(os.path.join("static/detected_img/", filename_extractor(imageIDs[i])), dpi=100, transparent=True, optimize=True, quality=90)
-                
+            # Write output to zipfile while removing original file
+            with ZipFile(DOWNLOAD_PATH + userID + '.zip', 'w') as zipObj:
+                for img in finalnames:
+                    zipObj.write(os.path.join(DOWNLOAD_PATH, img), img)
+                    os.remove(os.path.join(DOWNLOAD_PATH, img))
+                zipObj.write(os.path.join(DOWNLOAD_PATH, userID + ".xlsx"), userID + ".xlsx")
+
+            # Remove csv file
+            os.remove(os.path.join(DOWNLOAD_PATH, userID + ".xlsx"))
+
+            # Notify user of completion
+            msg = MIMEMultipart()
+            msg['From'] = "foxboundtnc@gmail.com"
+            msg['To'] = email
+            msg['Subject'] = "Download Link"
+            message = '''Your images have been processed. Please download the predictions by visiting the website https://127.0.0.1:5000/results/{}'''.format(userID)
+            msg.attach(MIMEText(message, 'plain'))
+            s = smtplib.SMTP('smtp.gmail.com', 587)
+            s.ehlo()
+            s.starttls()
+            s.login("foxboundtnc@gmail.com", "ASnowyDayInAmherst")
+            s.sendmail(msg['From'], email, msg.as_string())
+            s.quit()
+
+            # Remove userID from user queue and add to COMPLETE_QUEUE
+            db.ltrim(USER_QUEUE, 1, -1)
+
                 # Updating progress
                 # Generate key for progress linked to userID
-                progressID = str(userID) + "progress"
-                # Calculate percentage value
-                progress = []
-                percent = i/len(imageIDs) * 100.00
-                progress.append(percent)
-                msg = str(i) + "of" + str(len(imageIDs)) + "completed."
-                progress.append(msg)
-                db.set(progressID, progress)
+                # progressID = userIDs[i] + "progress"
+                # # Calculate percentage value
+                # progress = []
+                # percent = i/len(images) * 100.00
+                # progress.append(percent)
+                # msg = str(i) + "of" + str(len(images)) + "completed."
+                # progress.append(msg)
+                # db.set(progressID, percent)
 
             # Storing completed images under userID
-            db.set(userID, json.dumps(predictions))
-            
-            # Deleting userID from the processing queue
-            db.ltrim(IMAGE_QUEUE, 1, -1)
+            # db.set(userID, json.dumps(predictions))
         
-        # Sleep for half a second before polling again
-        time.sleep(0.5)
+        # Sleep for a second before polling again
+        time.sleep(1)
 
 if __name__ == "__main__":
     tf_classify()
--- flask/TNC2/app/app/web_server_test.py
+++ flask/TNC2/app/app/web_server_test.py
@@ -1,44 +1,68 @@
 from app import app
-from flask import render_template, request, make_response, session, url_for, redirect
-from flask import safe_join, send_from_directory, abort, flash, copy_current_request_context
+from flask import render_template, request, make_response, session, url_for, redirect, Response
+from flask import safe_join, send_file, send_from_directory, abort, flash, copy_current_request_context, stream_with_context
 from flask_dropzone import Dropzone
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_uploads import UploadSet, configure_uploads, IMAGES, ARCHIVES, patch_request_class
 from flask_executor import Executor
-import redis
-import os, uuid, pathlib, time, json, ntpath, random
-
-
+from redis import *
+from threading import Thread
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import os, uuid, pathlib, time, json, ntpath, random, sys
+import zipfile
 
 # Dropzone Settings
 app.config['DROPZONE_UPLOAD_MULTIPLE'] = True
 app.config['DROPZONE_ALLOWED_FILE_CUSTOM'] = True
-app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
-app.config['DROPZONE_MAX_FILE_SIZE'] = 100 * 1024 * 1024
-app.config['DROPZONE_REDIRECT_VIEW'] = 'results'
+app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*, application/zip, .zip, application/x-zip-compressed, multipart/x-zip'
+app.config['DROPZONE_MAX_FILE_SIZE'] = 10240 * 1024 * 1024 # 10GB for zip
+app.config['DROPZONE_PARALLEL_UPLOADS'] = 10000
 app.config['SECRET_KEY'] = 'SnowyDayInAmherst'
 app.config['EXECUTOR_TYPE'] = 'thread'
 app.config['EXECUTOR_MAX_WORKERS'] = 20
-
 app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(os.getcwd(), "app/static/uploaded_img/")
+app.config['UPLOADS_DEFAULT_DEST'] = os.path.join(os.getcwd(), "app/static/uploaded_img/")
+app.config['UPLOAD_PATH'] = os.path.join(os.getcwd(), "app/static/uploaded_img/")
 photos = UploadSet('photos', IMAGES)
+zips = UploadSet('zips', ARCHIVES)
 configure_uploads(app, photos)
-patch_request_class(app)
+# patch_request_class(app) # to limit upload size. default 16MB
 
 # Config parameters
-REDIS_HOST = "localhost"
+REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
 REDIS_DB = 0
 
-IMAGE_QUEUE = "image_queue"
 IMAGE_HEIGHT = 1920 * 0.75
 IMAGE_WIDTH = 1920
 IMAGE_DTYPE = "float32"
 
-BATCH_SIZE = 64 # Only for when Tensorflow GPU is used
+USER_QUEUE = "user_queue"
+USER_TOTAL = "user_total"
+USER_REMAINING = ""
+UPLOAD_PATH = os.path.join(os.getcwd(), "app/static/uploaded_img/")
+DOWNLOAD_PATH = os.path.join(os.getcwd(), "app/static/detected_img/")
+
+def mail(userID, email):
+    # Mail user saying that the images have been processed
+    msg = MIMEMultipart()
+    msg['From'] = "foxboundtnc@gmail.com"
+    msg['To'] = email
+    msg['Subject'] = "Submitted for Processing"
+    message = '''Your images have been submitted for processing. Please track and download the predictions by visiting the website https://127.0.0.1:5000/results/{}'''.format(userID)
+    msg.attach(MIMEText(message, 'plain'))
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.login("foxboundtnc@gmail.com", "ASnowyDayInAmherst")
+    s.sendmail(msg['From'], email, msg.as_string())
+    s.quit()
 
 # Connecting to redis
-db = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, 
+db = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, 
     db=REDIS_DB, decode_responses=True)
+# print(db.ping(), file=sys.stderr)
 
 # Home page. Pretty much static
 @app.route("/")
@@ -46,43 +70,119 @@ def index():
     return render_template("index.html")
 
 # Upload page. Contains dropzone code. Saves images to uploaded_img directory
-@app.route("/upload", methods=['GET', 'POST'])
+@app.route("/upload")
 def upload():
-
     # Note: Session may be avoided altogether but for now, leave as is
     # Generate user ID for user to track progress and retrieve download link
     # Set session for userID
+    # print("before userid and sess")
+    
+    session.clear()
+    # print(session)
     if "userID" not in session:
+        # print("no session yet", file=sys.stderr)
+        session.clear() # clearing flash messages from previous run if any
         session['userID'] = str(uuid.uuid4())
-        session['images'] = []
-    # List to hold our uploaded image IDs
-    imageIDs = []
+        session['images'] = False
+    return render_template("upload.html")
 
-    # If images are uploaded, save to upload directory and store in session
+@app.route("/upload", methods=["GET", "POST"])
+def upload_images():
+    # If images are uploaded, save to upload directory and push to user session in redis
     if request.method == 'POST':
-        file_obj = request.files
-        for f in file_obj:
-            file = request.files.get(f)
-            filename = photos.save(file, name=str(uuid.uuid4()) + ".")
+        for key, f in request.files.items():
+            # print(key, f, file=sys.stderr)
+            if key.startswith('file'):
+                print(f.filename)
+                if f.filename.rsplit(".", 1)[1] == "zip":
+                    with zipfile.ZipFile(f) as z:
+                        # Checking for jpg only to extract and push to redis
+                        for name in z.namelist():
+                            if name.rsplit(".", 1)[1].lower() == "jpg":
+                                session["images"] = True
+                                z.extract(name, os.path.join(UPLOAD_PATH))
+                                d = {"image":name}
+                                db.rpush(session["userID"], json.dumps(d))
+                else:
+                    session["images"] = True
+                    filename = photos.save(f) # Ensure strong naming convention
+                    # filename = photos.save(f, name=str(uuid.uuid4()) + ".")
+                    d = {"image":filename}
+                    db.rpush(session["userID"], json.dumps(d))
+    return render_template("upload.html")
 
-            # Note: Can save path or just filename. Let's try filename
-            # and build with that logic
-            imageIDs.append(photos.path(filename))
-            session['images'].append(photos.path(filename))
-        userID = str(uuid.uuid4())
-        # Flashing userID to user so that they can save it
-        flash("Please copy and save Task ID: " + userID)
-        # Store userID and imageIDs as a dictionary and push to redis
-        d = {"user_id": userID, "images": imageIDs, "progress": 0}
-        db.rpush(IMAGE_QUEUE, json.dumps(d))
-        return "Uploading to disk and Redis"
-    return render_template("upload.html") 
+@app.route("/process_redis", methods=['GET', 'POST'])
+def process_redis():
+    # Check if any images have been submitted
+    if session["images"]:
+        userID = session["userID"]
+        email = request.form['email']
 
-@app.route("/progress")
-def results():
-    # Check if userID in session. If not, redirect to upload page.
-    if "user_id" not in session:
-        return redirect(url_for('upload'))
-    # Get userID from session
-    userID = session
-    return "Well something is happening."
+        thread = Thread(target = mail, args = (userID, email, ))
+        thread.start()
+
+        # Flashing download link to user
+        flash("/results/" + userID)
+        # Push user request to USER_QUEUE to be polled by model server
+        d = {"userID": userID, "email": email}
+        db.rpush(USER_QUEUE, json.dumps(d))
+    else:
+        flash("Please upload images before starting!")
+    # Clear session for next run
+    [session.pop(key) for key in list(session.keys()) if key != '_flashes']
+    return render_template("process_redis.html")
+
+@app.route("/results/<userID>")
+def result(userID):
+    # Check if download file present in local system. If not send to tracking page
+    # print("yup")
+    if os.path.isfile(os.path.join(DOWNLOAD_PATH, userID + ".zip")):
+        return send_from_directory(DOWNLOAD_PATH, filename=userID + ".zip")
+    else:
+        session["tempID"] = userID
+        users = db.lrange(USER_QUEUE, 0, -1)
+        print(users)
+        cu, ci = 0, 0
+        for user in users:
+            user = json.loads(user)["userID"]
+            ci += len(db.lrange(user, 0, -1))
+            print(ci)
+            if user == userID:
+                et = ci * 10
+                return render_template("results.html", users = cu, images=ci, et=et)
+                break
+            cu += 1
+        # abort(404)
+        return response("Invalid request. Your link expired.", 500);
+
+# Remove comment section if you wanna add progress bar instead of countdown timer
+# Progress is continiously streamed to client so it can get data heavy if there are many users. Unlikely to happen with FoxBound
+# @app.route('/progress')
+# def progress():
+#     def generate():
+#         user = session["tempID"]
+#         ti = len(db.lrange(user, 0, -1))
+#         ci = ti
+#         x = 0.0
+        
+#         while ci != 0:
+#             et, cu, tti = 0, 0, 0
+#             users = db.lrange(USER_QUEUE, 0, -1)
+#             for u in users:
+#                 u = json.loads(u)["userID"]
+#                 tti += len(db.lrange(u, 0, -1))
+#                 print(ci)
+#                 if user == u:
+#                     et = tti * 10 / 60
+
+#             ci = len(db.lrange(user, 0, -1))
+#             print(ci)
+#             try:
+#                 prog = (ti - ci) / ti * 100
+#             except:
+#                 prog = 0
+#             ret = "data:{\nprog:" +str(prog) + ",\nusers:" + str(cu) + ",\nimages:" +str(images) + ",\net:" + str(et) + "}\n\n"
+#             yield ret
+
+#             time.sleep(10)
+#     return Response(stream_with_context(generate()), mimetype='text/event-stream')
