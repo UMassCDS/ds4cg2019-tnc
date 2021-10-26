516b38154dc2bd8e54f691177eb7e105c44559f3
Paige Gulley <nano3.14@gmail.com>
2021-10-26 19:03:57 -0400

    First commit to this branch. On flask/pgulley/app is a fresh webapp skeleton, slowly getting filled out. Just assembling the functional components rn

--- flask/PGulley/app/static/main.js
+++ flask/PGulley/app/static/main.js
@@ -1,13 +1,111 @@
 
-	$("#test_ajax").click(function(){
+//this just confirms that we can bounce info back and forth with lambda
+$("#test_ajax").click(function(){
+	$.ajax({
+		url:window.location.href+"/ajax_test",
+		data:{name:"nothin"}
+	}).done(function(resp){		
+		$("#sample_body").append(`<tr><td>${resp.status}</td><td>${resp.data}</td></tr>`);
+	});
+})
+
+//this route tests putting something into the dynamodb instance
+$("#put_doc").click(function(){
+	var document_text = $("#doc_text")[0].value
+	$("#doc_text").val("")
+	$.ajax({
+		url:window.location.href+"/put_doc",
+		data:{doc_text:document_text}
+	}).done(function(resp){
+		$("#ddb_table_body").append(`<tr><td>${resp.status}</td><td>${resp.doc_text}</td></tr>`);
+	})
+})
+
+//Just a rendering handle placeholder while I'm assembling the functionality
+job_line = function(job){
+	if(job.doc_text != undefined){
+		return `<tr><td>${job.doc_text.S}</td><td>${new Date(job.timestamp.N*1000).toString()}</td></tr>`
+	}else{
+		
+		return `<tr><td>${job.upload_location.S}</td><td>${new Date(job.timestamp.N*1000).toString()}</td></tr>`
+	}
+	
+}
+
+//Calls the poll route on the lambda monolith, returns all of the relevant documents
+poll_ddb = function(){
+	$.ajax({
+		url:window.location.href+"/poll_ddb"
+	}).done(function(resp){
+		table = $("#ddb_poll_table")
+		table.empty()
+		for( const job_record of resp["items"]){
+			table.append(job_line(job_record))
+		}
+	})
+}
+
+poll_ddb() //call pollddb on load
+setInterval(poll_ddb, 1000*60*10) //and also then every 10 minutes
+
+
+
+///S3 upload concerns:
+//- user uploads the file to the browser
+//- user presses 'upload'- the browser asks the server for a signed aws url
+//- the browesr recieves the signed url, then PUTs to it
+//- the browser recieves status 200- tells the server to create a ddb entry
+file_to_upload = ""
+fname = ""
+
+$("#file_upload").change(function(){
+	reader = new FileReader()
+	reader.onload = function(e){
+		//do validation here
+		file_to_upload = e.target.result
+	}
+	reader.readAsDataURL($(this)[0].files[0])
+	fname = $(this)[0].files[0].name
+})
+
+$("#do_upload").click(function(){
+	if(file_to_upload != ""){
 		$.ajax({
-			url:"/ajax_test",
-			data:{name:"nothin"}
+			url:window.location.href+"/get_s3_upload_url",
+			data:{filename:fname}
 		}).done(function(resp){
 			
-			$("#sample_body").append(`<tr><td>${resp.status}</td><td>${resp.data}</td></tr>`);
-		});
-	})
+			///this is adapted from an official amazon example.
+			// it reads the data in, peels of the header,
+			// and transforms it into an array of b64 bytes
+			
+			binary = atob(file_to_upload.split(",")[1]) 
+			array = []
+			for (var i = 0; i < binary.length; i++) {
+	            array.push(binary.charCodeAt(i))
+	        }
+	       	blobData = new Blob([new Uint8Array(array)], {type: 'zip'})
+
+			fetch(resp.uploadURL,{
+				 
+				method:"PUT",
+				body:blobData
+			
+			}).then(function(resp){
+				if(resp.status == 200){
+					$.ajax({
+						url:window.location.href+"/put_job_record_ddb",
+						data:{location:resp.url.split("?")[0]}
+					}).done(function(resp){
+						console.log("whole loop complete")
+						poll_ddb()
+					})
+				}
+			})
+
+		})
+	}
+})
 
 
 
---
+++ flask/PGulley/app/tnc_app.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, url_for, request
+import json, uuid, time, hashids
+import boto3
+
+settings = json.load(open("app_settings.json"))
+
+ddb_client = boto3.client("dynamodb", "us-east-2")
+s3_client = boto3.client("s3", "us-east-2")
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+
+	post_url = s3_client.generate_presigned_post("tnc-test-app", "test")
+	
+	return render_template("main.html", 
+		style_link = url_for("static", filename="style.css"),
+		js_link = url_for("static", filename="main.js")
+		)
+
+@app.route('/ajax_test')
+def ajax_test():
+	return {'status':"OK", 'data':"test"}
+
+#an ajax route
+@app.route('/put_doc')
+def put_document():
+	item = {
+			'job_id':{"S": uuid.uuid1().hex},
+			'user_id':{"S":"test_id"},
+			'doc_text':{"S":request.args.get("doc_text")},
+			'timestamp':{"N":f'{time.time()}'}
+	}
+	ddb_client.put_item(
+		TableName = settings["JOB_TABLE"],
+		Item = item)
+	return {"status":"OK", "doc_text":item["doc_text"]["S"]}
+
+
+#an ajax route
+@app.route("/poll_ddb")
+def poll_ddb():
+	
+	response = ddb_client.query(
+		TableName = settings["JOB_TABLE"],
+		IndexName = settings["USR_INDEX"],
+		Select = "ALL_ATTRIBUTES",
+		KeyConditionExpression = "user_id = :user_id_val",
+		ExpressionAttributeValues = {":user_id_val": {"S": "test_id"}}
+	)
+	
+	#just get all the documents currently in the ddb and return them raw.
+	#hypothetically this will query by user_id once that's implemented
+
+	return {"items": response["Items"]}
+
+
+#ajax route
+@app.route("/get_s3_upload_url")
+def get_s3_upload_url():
+	fname = request.args.get("filename").split(".")
+	hasher = hashids.Hashids()
+	timehash = hasher.encode(int(time.time()*10))
+	#s3 key is the original file name plus a hash of the current time.
+	#just to prevent collisions. 
+	full_key = f'{fname[0]}-{timehash}.{fname[1]}'
+	post_url = s3_client.generate_presigned_url("put_object",
+			{"Bucket":settings["S3_BUCKET"], "Key":full_key})
+	return {"uploadURL":post_url}
+
+@app.route("/put_job_record_ddb")
+def put_job_record_ddb():
+	location = request.args.get("location")
+	item = {
+		'job_id':{"S": uuid.uuid1().hex},
+		'user_id':{"S":"test_id"},
+		'upload_location':{"S":location},
+		'timestamp':{"N":f'{time.time()}'}
+	}
+	ddb_client.put_item(
+		TableName = settings["JOB_TABLE"],
+		Item = item
+	)
+	return {"Status":"OK"}
+
+#ajax path - runs on user interactions
+#def login
+#	recieve login attempt
+#		if successfull, register login and redirect browser
+#	else
+#		send error back.
+
+
+
+if __name__ == "__main__":
+	app.run()	
